2e34ca217fe590e1b8bb7509e54b25a10ad39a51
satyakaam <satyakaam.parikh@gmail.com>
2020-06-14 00:48:17 -0400

    heroku seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,7 +11,7 @@ month = Time.current.month
 User.all.each do |user|
   for day in 1..Time.current.day-1
     [6,11,12,17].each_with_index do |hour, index|
-      time = Time.new(year, month, day, hour, 0, 0)
+      time = Time.new(year, month, day, hour, 0, 0, Time.current.formatted_offset)
       event = user.events.create(occurred_at: time, event_type: (index+1).odd? ? :check_in : :check_out)
     end
   end
